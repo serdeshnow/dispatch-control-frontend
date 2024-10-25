d78bb158b503365399143550f36d8eb789af335a
serdeshnow <vserdes1977@mail.ru>
2024-10-26 01:35:39 +0300

    feat: upload pages

---
+++ app/src/pages/MonitoringPage/MonitoringPage.tsx
@@ -0,0 +1,10 @@
+// import './MonitoringPage.scss';
+import { Container, Typography } from '@mui/material';
+
+export const MonitoringPage = () => {
+	return (
+		<Container sx={{height:1}}>
+			<Typography variant="h5" component="div">Monitoring page</Typography>
+		</Container>
+	);
+};
---
+++ app/src/pages/ReportPage/ReportPage.tsx
@@ -0,0 +1,11 @@
+// import './ReportPage.scss';
+import { Container, Typography } from '@mui/material';
+
+export const ReportPage = () => {
+	return (
+		<Container sx={{height:1}}>
+			<Typography variant="h5" component="div">Report page</Typography>
+
+		</Container>
+	);
+};
