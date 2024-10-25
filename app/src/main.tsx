703b5102cccecccd8f84c861018d26d44ec96811
serdeshnow <vserdes1977@mail.ru>
2024-10-25 23:53:17 +0300

    feat: mui theme added

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,18 +4,23 @@ import { App } from './App.tsx';
 import './styles/index.css';
 import './styles/variables.css';
 import './styles/normalize.css';
-import './styles/class-names.scss';
+import './styles/fonts.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/store.ts';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { theme } from './theme.ts';
 
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<Provider store={store}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
+			<ThemeProvider theme={theme}>
+				<CssBaseline />
+					<BrowserRouter>
+						<App />
+					</BrowserRouter>
+			</ThemeProvider>
 		</Provider>
 	</StrictMode>,
 );
--- app/src/pages/DashboardPage/DashboardPage.tsx
+++ app/src/pages/DashboardPage/DashboardPage.tsx
@@ -2,7 +2,7 @@ import { Container } from '@mui/material';
 
 export const DashboardPage = () => {
 	return (
-		<Container sx={{height:1}}> 
+		<Container sx={{height:1}}>
 		</Container>
 	);
 };
